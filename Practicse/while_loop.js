413ae3d106ffe5b51f8a9d2e938e0f3fb8219244
shashank kumar singh <53405502+shashank12237@users.noreply.github.com>
2022-12-23 21:59:19 +0530

    basics of while loop

---
+++ Practicse/while_loop.js
@@ -0,0 +1,18 @@
+var num = 1;
+while(num<=5)
+{
+    console.log("sagar");
+    num++;
+}
+var num1=1;
+while(num1<=5)
+{
+    console.log("is a");
+    num1++;
+}
+var num2=1;
+while(num2<=5)
+{
+    console.log("Randwa");
+    num2++;
+}
