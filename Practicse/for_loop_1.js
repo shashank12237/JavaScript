419e9174a1945bb489aad39ba8f57381c4097927
shashank kumar singh <53405502+shashank12237@users.noreply.github.com>
2022-12-23 22:49:55 +0530

    for loop ist

---
+++ Practicse/for_loop_1.js
@@ -0,0 +1,24 @@
+let i=1;
+let j=0;
+let string="";
+for(i=1;i<=5;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+       string+="*";
+    }
+    string+="\n";
+}
+console.log(string);
+// let k=1;
+// let l=0;
+// let string1="";
+// for(k=1;k<=5;k++)
+// {
+//     for(l=k;l<=5;l--)
+//     {
+//        string1+="*";
+//     }
+//     string1+="\n";
+// }
+// console.log(string1);
